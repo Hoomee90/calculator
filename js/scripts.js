eb3bc5142b99c1b0d8b7c2c3e65d5f96d39faa64
Samantha Callie <Hoomee90@gmail.com>
2023-07-19 14:13:45 -0700

    add function to preform all operations at once

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,12 @@ function divide(num1, num2) {
   return parseInt(num1) / parseInt(num2);
 }
 
+function allMath(num1, num2) {
+  return ("The sum of those numbers are " + add(num1, num2) + ". The difference of those numbers are " + subtract(num1, num2) + ". The product of those numbers are " + multiply(num1, num2) + ". The quotient of those numbers is " + divide(num1, num2) + ".");
+}
+
 // user interface logic
+
 const addnum1 = prompt("Enter a number to add:");
 const addnum2 = prompt("Enter another number to add:");
 
@@ -35,3 +40,8 @@ const divnum1 = prompt("Enter a number to divide:");
 const divnum2 = prompt("Enter another number to divide:");
 
 window.alert("The quotient is " + divide(divnum1, divnum2) + ".");
+
+const allnum1 = (prompt("Enter a number you would like do at once:"));
+const allnum2 = (prompt("Enter another number you would like to do all at once:"));
+
+window.alert(allMath(allnum1, allnum2));
