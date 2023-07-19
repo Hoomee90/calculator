561b4d0ed0c5e4cf6ca4c6df5668e93021f5d747
Samantha Callie <Hoomee90@gmail.com>
2023-07-19 13:49:51 -0700

    add logic for subtraction, multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,35 @@ function add(num1, num2) {
   return parseInt(num1) + parseInt(num2);
 }
 
+function subtract(num1, num2) {
+  return parseInt(num1) - parseInt(num2);
+}
+
+function multiply(num1, num2) {
+  return parseInt(num1) * parseInt(num2);
+}
+
+function divide(num1, num2) {
+  return parseInt(num1) / parseInt(num2);
+}
+
 // user interface logic
-const addnum1 = prompt("Enter a number:");
-const addnum2 = prompt("Enter another number:");
+const addnum1 = prompt("Enter a number to add:");
+const addnum2 = prompt("Enter another number to add:");
 
 window.alert(add(addnum1, addnum2));
+
+const subnum1 = prompt("Enter a number to subtract:");
+const subnum2 = prompt("Enter another number to add:");
+
+window.alert(subtract(subnum1, subnum2));
+
+const multnum1 = prompt("Enter a number to multiply:");
+const multnum2 = prompt("Enter another number to add:");
+
+window.alert(multiply(multnum1, multnum2));
+
+const divnum1 = prompt("Enter a number to divide:");
+const divnum2 = prompt("Enter another number to add:");
+
+window.alert(divide(divnum1, divnum2));
