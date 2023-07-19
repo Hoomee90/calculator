a9974f81ef005d91567bf5039b49608c8b2f9855
Samantha Callie <Hoomee90@gmail.com>
2023-07-19 13:55:07 -0700

    add more text to page alerts

--- js/scripts.js
+++ js/scripts.js
@@ -19,19 +19,19 @@ function divide(num1, num2) {
 const addnum1 = prompt("Enter a number to add:");
 const addnum2 = prompt("Enter another number to add:");
 
-window.alert(add(addnum1, addnum2));
+window.alert("The sum is " + add(addnum1, addnum2) + ".");
 
 const subnum1 = prompt("Enter a number to subtract:");
-const subnum2 = prompt("Enter another number to add:");
+const subnum2 = prompt("Enter another number to subtract:");
 
-window.alert(subtract(subnum1, subnum2));
+window.alert("The difference is " + subtract(subnum1, subnum2) + ".");
 
 const multnum1 = prompt("Enter a number to multiply:");
-const multnum2 = prompt("Enter another number to add:");
+const multnum2 = prompt("Enter another number to multiply:");
 
-window.alert(multiply(multnum1, multnum2));
+window.alert("The product is " + multiply(multnum1, multnum2) + ".");
 
 const divnum1 = prompt("Enter a number to divide:");
-const divnum2 = prompt("Enter another number to add:");
+const divnum2 = prompt("Enter another number to divide:");
 
-window.alert(divide(divnum1, divnum2));
+window.alert("The quotient is " + divide(divnum1, divnum2) + ".");
